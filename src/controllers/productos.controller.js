9fdfd8985450a76a9d16c56ceff1e6fba90579b1
JuanjoMcLoughlin <juan.jose.mc.loughlin@gmail.com>
2024-10-11 16:39:00 -0300

    Tercer video, Controladores

---
+++ src/controllers/productos.controller.js
@@ -0,0 +1,21 @@
+const querystring = require('querystring');
+
+const index = (req,res)=>{
+    //const limit = req.query.limit;
+    const query = querystring.stringify(req.query);
+    fetch('https://fakestoreapi.com/products?'+query)
+    .then(res=>res.json())
+    .then(productos=>res.send(productos));
+};
+
+const show = (req,res)=>{
+    fetch('https://fakestoreapi.com/products/'+req.params.id)
+    .then(res=>res.json())
+    .then(producto=>res.send(producto));//da la respuesta en formato 
+};
+
+
+module.exports = {
+    index,
+    show
+}
--- src/routes/productos.router.js
+++ src/routes/productos.router.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const querystring = require('querystring');
+const controller = require('../controllers/productos.controller');
 
-router.get('/productos',(req,res)=>{
-    //const limit = req.query.limit;
-    const query = querystring.stringify(req.query);
-    fetch('https://fakestoreapi.com/products?'+query)
-    .then(res=>res.json())
-    .then(productos=>res.send(productos));
-});
+router.get('/productos', controller.index);
 
-
-router.get('/productos/:id',(req,res)=>{
-    fetch('https://fakestoreapi.com/products/'+req.params.id)
-    .then(res=>res.json())
-    .then(producto=>res.send(producto));//da la respuesta en formato 
-});
-
-
-
+router.get('/productos/:id', controller.show);
 
 
 module.exports = router;
